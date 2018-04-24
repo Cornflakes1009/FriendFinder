2224ed92cb50f434073f2916cb55bf098d88fab9
Harold Davidson <harold.davidson89@gmail.com>
2018-04-24 13:02:03 -0500

    done

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,9 @@
 
-var friends = [
+var friendsArr = [
     {
-        "name":"Harold",
-        "photo":"https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/13100926_10154172237269894_2307491505043615992_n.jpg?_nc_cat=0&oh=93de7ce021030ae0c2ae329866fc9d94&oe=5B5E75B0",
-        "scores":[
+        "name": "Michael Kelso",
+        "photo": "https://pbs.twimg.com/profile_images/2586770390/cfzmd8muywpcoryg69e3.jpeg",
+        "scores": [
             "4",
             "4",
             "4",
@@ -14,8 +14,120 @@ var friends = [
             "4",
             "4",
             "4"
-            ]
-        },
+        ]
+    },
+    {
+        "name": "Red Forman",
+        "photo": "https://img.buzzfeed.com/buzzfeed-static/static/2015-12/16/17/campaign_images/webdr02/21-times-red-forman-was-the-realest-fucker-on-the-2-19311-1450304761-1_dblbig.jpg",
+        "scores": [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        "name": "Jackie Burkhart",
+        "photo": "https://vignette.wikia.nocookie.net/that70sshow/images/d/db/Jackie.JPG/revision/latest?cb=20130421211315",
+        "scores": [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Eric Forman",
+        "photo": "http://www4.pictures.zimbio.com/mp/MSAQDom-66-l.jpg",
+        "scores": [
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2"
+        ]
+    },
+    {
+        "name": "Steven Hyde",
+        "photo": "https://thumbs.gfycat.com/SaneHappyGonolek-mobile.jpg",
+        "scores": [
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "Kitty Forman",
+        "photo": "https://pbs.twimg.com/profile_images/1832404800/kittyforman_400x400.jpeg",
+        "scores": [
+            "5",
+            "4",
+            "3",
+            "2",
+            "1",
+            "5",
+            "4",
+            "3",
+            "2",
+            "1"
+        ]
+    },
+    {
+        "name": "Fez",
+        "photo": "https://thumbs.gfycat.com/SaneHappyGonolek-mobile.jpg",
+        "scores": [
+            "1",
+            "1",
+            "3",
+            "3",
+            "1",
+            "1",
+            "3",
+            "3",
+            "1",
+            "1"
+        ]
+    },
+    {
+        "name": "Donna ",
+        "photo": "https://data.whicdn.com/images/298194389/large.png",
+        "scores": [
+            "5",
+            "4",
+            "5",
+            "3",
+            "5",
+            "1",
+            "2",
+            "5",
+            "1",
+            "5"
+        ]
+    }
 ];
 
-module.exports = friends;
+module.exports = friendsArr;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,14 +6,35 @@ module.exports = function (app) {
     // A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
     app.get("/api/friends", function (req, res) {
         res.sendFile(path.join(__dirname, "../../app/data/friends.js"));
+        return res.json(friendsList);
     });
 
     // route two 
     // A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
     app.post("/api/friends", function (req, res) {
-        console.log(req.body);
-        friendsList.push('req: ' + req.body);
-        
+        var surveyData = req.body;
+        var responses = surveyData.scores;
+        var nameOfMatch = '';
+        var matchPhoto = '';
+        var totalDifference = 51;
+
+        for (var i = 0; i < friendsList.length; i++) {
+            var diff = 0;
+            for (var j = 0; j < responses.length; j++) {
+                diff += Math.abs(friendsList[i].scores[j] - responses[j]);
+            }
+            // If lowest difference, set Match
+            if (diff < totalDifference) {
+                totalDifference = diff;
+                nameOfMatch = friendsList[i].name;
+                matchPhoto = friendsList[i].photo;
+            }
+        }
+        // Add new user
+        friendsList.push(surveyData);
+
+        // Send response to modal
+        res.json({ status: 'OK', nameOfMatch: nameOfMatch, matchPhoto: matchPhoto });
     });
-    
+
 };
